13fbb6935e61eaf49178ab6678142c98db09fc78
Alexandr <alexandrfenske@gmail.com>
2025-01-24 20:03:31 +0300

    page  film collections

---
+++ src/components/UI/MoviesList/MoviesList.jsx
@@ -0,0 +1,28 @@
+import { Pagination, Stack } from '@mui/material';
+
+import MovieCard from '../MovieCard/MovieCard';
+
+const MoviesList = ({ movies, totalPages, page, setPage }) => {
+  return (
+    <>
+      <Stack direction="row" justifyContent="center" flexWrap="wrap">
+        {movies.map(movie => (
+          <MovieCard key={movie.kinopoiskId} movie={movie} />
+        ))}
+      </Stack>
+      <Stack alignItems="center">
+        <Pagination
+          count={totalPages}
+          color="primary"
+          variant="outlined"
+          shape="rounded"
+          size="large"
+          page={page}
+          onChange={(_, value) => setPage(value)}
+        />
+      </Stack>
+    </>
+  );
+};
+
+export default MoviesList;
--- src/components/pages/MoviesListTop/MoviesListTop.jsx
+++ src/components/pages/MoviesListTop/MoviesListTop.jsx
@@ -1,13 +1,44 @@
+import { ArrowBack } from '@mui/icons-material';
+import { Button, Stack, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { TOP_LISTS } from '../../../../constants';
 import { useGetFilmsTopQuery } from '../../../services/kinopoiskAPI';
+import MoviesList from '../../UI/MoviesList';
 
 const MoviesListTop = () => {
-  const { data, error, isLoading } = useGetFilmsTopQuery({
-    type: 'TOP_POPULAR_ALL',
-    page: 1,
-  });
-  console.log(data, error, isLoading);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [page, setPage] = useState(1);
 
-  return <>MoviesListTop</>;
+  const movieType = TOP_LISTS.find(el => el.url === location.pathname);
+
+  const { data, error, isLoading } = useGetFilmsTopQuery({
+    type: movieType.value,
+    page,
+  });
+
+  useEffect(() => {
+    setPage(1);
+  }, [location]);
+
+  if (error) return <p>Some error</p>;
+  if (isLoading) return <p>Loading...</p>;
+  return (
+    <>
+      <Stack flexDirection="row" sx={{ mt: 2, mb: 2 }}>
+        <Button startIcon={<ArrowBack />} onClick={() => navigate(-1)} />
+        <Typography variant="h4">{movieType.title}</Typography>
+      </Stack>
+      <MoviesList
+        movies={data.items}
+        totalPages={data.totalPages}
+        page={page}
+        setPage={setPage}
+      />
+    </>
+  );
 };
 
 export default MoviesListTop;
